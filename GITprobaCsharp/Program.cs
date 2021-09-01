4d49dcb0a59267f25d5f07f5b604cdabf055f7d3
LI <LI@DESKTOP-VDOGF3H>
2021-09-01 21:48:25 +0200

    Add project files.

---
+++ GITprobaCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GITprobaCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
